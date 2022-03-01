a78fa957409dc6f5299a3d21c188b33f6e3ab4ff
Holly Clibbon <vendorofdoom@users.noreply.github.com>
2022-03-01 18:47:43 +0000

    Add some basic variation to the station buildings

---
+++ APG_Assignment_1/Assets/StationBuildingGenerator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationBuildingGenerator : MonoBehaviour
+{
+    public GameObject[] RoofPrefabs;
+    public GameObject[] BuildingPrefabs;
+    public GameObject[] FacadePrefabs;
+    public GameObject[] CloudPrefabs;
+
+    public Transform parent;
+
+    private void Start()
+    {
+        GameObject.Instantiate(RoofPrefabs[Random.Range(0, RoofPrefabs.Length)], parent);
+        GameObject.Instantiate(BuildingPrefabs[Random.Range(0, BuildingPrefabs.Length)], parent);
+        GameObject.Instantiate(FacadePrefabs[Random.Range(0, FacadePrefabs.Length)], parent);
+        GameObject.Instantiate(CloudPrefabs[Random.Range(0, CloudPrefabs.Length)], parent);
+    }
+}
