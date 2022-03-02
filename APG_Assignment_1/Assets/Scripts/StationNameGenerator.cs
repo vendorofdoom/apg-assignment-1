dac46b749618239a0b9afc7ac0cf956a12782d34
Holly Clibbon <vendorofdoom@users.noreply.github.com>
2022-03-02 14:49:46 +0000

    Edits to station name generator

--- APG_Assignment_1/Assets/Scripts/StationNameGenerator.cs
+++ APG_Assignment_1/Assets/Scripts/StationNameGenerator.cs
@@ -7,7 +7,6 @@ public class StationNameGenerator : MonoBehaviour
     public List<string> adjectives;
     public List<string> prefixes;
     public List<string> suffixes;
-    public List<string> places;
 
     public string GenerateName()
     {
@@ -16,10 +15,6 @@ public class StationNameGenerator : MonoBehaviour
         if (picker <= 0.25f)
         {
             return adjectives[Random.Range(0, adjectives.Count)] + " " + prefixes[Random.Range(0, prefixes.Count)] + suffixes[Random.Range(0, suffixes.Count)];
-        }
-        else if (picker > 0.25f && picker <= 0.5f)
-        {
-            return prefixes[Random.Range(0, prefixes.Count)] + suffixes[Random.Range(0, suffixes.Count)] + " " + places[Random.Range(0, places.Count)];
         }
         else
         {
